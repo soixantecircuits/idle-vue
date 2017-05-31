264b6f4c41fe43a88a461c38e25786250eb22f2c
Olivier FAURE <olivier.faure@epitech.eu>
2017-05-31 11:24:04 +0200

    :bug: Fix IdleView CSS

--- src/Idle.vue
+++ src/Idle.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="idle-view" :class="{active: isAppIdle}">
-    <div class="overlay" :class="{active: isAppIdle}"></div>
+  <div class="idle-view" :class="{active: isIdle}">
+    <div class="overlay"></div>
     <sprite spriteId="touch"
       :spriteSrc="require('../assets/touch.png')"
       :spriteW='180'
@@ -26,49 +26,51 @@ export default {
     this.$refs.sprite.stop()
   }
 }
-
 </script>
 
 <style>
-  .idle-vue {
-    display: none;
-  }
-  .idle-vue.active {
-    pointer-events: all;
-    display: block;
-  }
-  .idle-vue .sprite {
-    top: 700px;
-    bottom: 0px;
-    margin: auto;
-    position: absolute;
-    height: 10px;
-    width: 180px;
-    left: 0px;
-    right: 0px;
-    z-index: 9999;
-    -webkit-transform: scale(0.7);
-  }
-  .idle-vue .overlay {
-    width: 100vw;
-    height: 100vh;
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    background: #000;
-    pointer-events:none;
-    z-index: 8888;
-    opacity: 0;
-    /*-webkit-animation: SlowMo 5s cubic-bezier(0.77, 0, 0.175, 1) infinite;*/
-    -webkit-transition: opacity 800ms cubic-bezier(0.77, 0, 0.175, 1);
-  }
-  .idle-vue .overlay.active {
-    pointer-events: all;
-    opacity: 0.6;
-  }
-  @-webkit-keyframes SlowMo {
-    0%{background-position:0% 50%}
-    50%{background-position:100% 50%}
-    100%{background-position:0% 50%}
-  }
+.idle-view {
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 8888;
+
+  pointer-events: none;
+  display: none;
+}
+.idle-view.isIdle {
+  pointer-events: all;
+  display: block;
+}
+.idle-view .sprite {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+
+  height: 10px;
+  width: 180px;
+  z-index: 9999;
+  -webkit-transform: scale(0.7);
+}
+.idle-view .overlay {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 8888;
+
+  background: #000;
+  opacity: 0;
+  /*-webkit-animation: SlowMo 5s cubic-bezier(0.77, 0, 0.175, 1) infinite;*/
+  -webkit-transition: opacity 800ms cubic-bezier(0.77, 0, 0.175, 1);
+}
+.idle-view.isIdle .overlay {
+  opacity: 0.6;
+}
+@-webkit-keyframes SlowMo {
+  0%{background-position:0% 50%}
+  50%{background-position:100% 50%}
+  100%{background-position:0% 50%}
+}
 </style>
