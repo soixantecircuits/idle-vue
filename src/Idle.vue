8fd2f7a82cd33ca403c70b51355523a9e227fd71
Olivier FAURE <olivier.faure@epitech.eu>
2017-06-07 11:36:28 +0200

    :bug: Fix default idle screen not appearing

--- src/Idle.vue
+++ src/Idle.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="idle-view" :class="{active: isIdle}">
+  <div class="idle-view" :class="{isIdle: isIdle}">
     <div class="overlay"></div>
     <sprite spriteId="touch"
       :spriteSrc="require('../assets/touch.png')"
